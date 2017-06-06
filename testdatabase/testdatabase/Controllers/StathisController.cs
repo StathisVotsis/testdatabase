ff547c4e3f6071fd08332d0cb4d13fac219aef8a
StathisVotsis <stathisvotsis@gmail.com>
2017-06-06 16:24:21 +0300

    hellascommit

---
+++ testdatabase/testdatabase/Controllers/StathisController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace testdatabase.Controllers
+{
+    [RoutePrefix("api/Test")]
+    public class StathisController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            
+            return Request.CreateResponse(HttpStatusCode.OK, "Stathis ok");
+           
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(int id)
+        {
+            int i = id + 1;
+            return Request.CreateResponse(HttpStatusCode.OK, i);
+
+        }
+    }
+}
